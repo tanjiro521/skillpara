eb427d20daf9d31e8127f8df1efdc9798e7773a0
Param <parambanerjee021@gmail.com>
2026-04-30 17:22:57 +0530

    Fix Turbopack conflict by removing custom webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -13,23 +13,7 @@ const nextConfig = {
   productionBrowserSourceMaps: false,
   // Disable React strict mode which can lead to issues with undefined arrays during development
   reactStrictMode: false,
-  // Disable webpack source maps entirely
-  webpack: (config, { isServer, dev }) => {
-    // Disable source maps in production
-    if (!dev) {
-      config.devtool = false;
-      
-      // Disable source maps for minimized files
-      if (config.optimization && config.optimization.minimizer) {
-        config.optimization.minimizer.forEach((minimizer) => {
-          if (minimizer.constructor.name === 'TerserPlugin') {
-            minimizer.options.terserOptions.sourceMap = false;
-          }
-        });
-      }
-    }
-    return config;
-  },
+
 }
 
 export default nextConfig
